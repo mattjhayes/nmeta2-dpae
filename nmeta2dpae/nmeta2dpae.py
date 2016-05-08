679d456350c2ee6b3c7384bf3b6216f566a3f8e8
mattjhayes <matthew_john_hayes@hotmail.com>
2016-05-08 21:52:09 +1200

    Changed test file to format that is compatible with py.test and updated version number to 0.3.0

--- nmeta2dpae/nmeta2dpae.py
+++ nmeta2dpae/nmeta2dpae.py
@@ -50,7 +50,7 @@ class DPAE(object):
         Initialise the DPAE class
         """
         #*** Version number for compatibility checks:
-        self.version = '0.2.0'
+        self.version = '0.3.0'
 
         #*** Instantiate config class which imports configuration file
         #*** config.yaml and provides access to keys/values:
