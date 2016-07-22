d74f5861e8041a9f8f656c4e03dd01b7495e843f
mattjhayes <matthew_john_hayes@hotmail.com>
2016-07-22 19:22:28 +1200

    Updated version to 0.3.4

--- nmeta2dpae/nmeta2dpae.py
+++ nmeta2dpae/nmeta2dpae.py
@@ -50,7 +50,7 @@ class DPAE(object):
         Initialise the DPAE class
         """
         #*** Version number for compatibility checks:
-        self.version = '0.3.3'
+        self.version = '0.3.4'
 
         #*** Instantiate config class which imports configuration file
         #*** config.yaml and provides access to keys/values:
