6cdadbec4015e62022cec98b926dbefcb8635c73
mattjhayes <matthew_john_hayes@hotmail.com>
2016-09-30 21:00:43 +1300

    Path fix for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../nmeta2dpae'))
-sys.path.insert(0, os.path.abspath('../../nmeta2dpae/classifiers'))
+#sys.path.insert(0, os.path.abspath('../../nmeta2dpae'))
+#sys.path.insert(0, os.path.abspath('../../nmeta2dpae/classifiers'))
 
 # -- General configuration ------------------------------------------------
 
